8c1eb5bb16ba713055684468db2f1b15647e0100
Olivia Fontanese <olfo4481@colorado.edu>
2018-10-19 21:39:18 -0600

    logic removed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,16 +5,3 @@ function timesUp() {
 }
 </script>
 
-<script>
-function valueChanged() {
-    if (document.getElementById("is_BMW").checked == true) {
-        document.getElementById("is_BMW").value = 1;
-        document.getElementById("is_Mercedes").value = 0;
-    } else {
-        document.getElementById("is_BMW").value = 0;
-        document.getElementById("is_Mercedes").value = 1;
-    }
-    console.log(document.getElementById("is_BMW").value);
-    console.log(document.getElementById("is_Mercedes").value)
-}
-</script>
